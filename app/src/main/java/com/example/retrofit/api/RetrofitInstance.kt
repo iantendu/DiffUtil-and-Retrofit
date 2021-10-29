4fc4a9bf6bea1f7f7886cd9126db27d36388700a
iantendu <iantendu@gmail.com>
2021-10-29 17:25:23 +0300

    adding logging interceptor and okthttp

--- app/src/main/java/com/example/retrofit/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/retrofit/api/RetrofitInstance.kt
@@ -1,5 +1,7 @@
 package com.example.retrofit.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,9 +10,15 @@ const val BASE_URL="https://jsonplaceholder.typicode.com/"
 object RetrofitInstance {
 
     val api:TodoApi by lazy {
+      val logging=HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client=OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(TodoApi::class.java)
     }
